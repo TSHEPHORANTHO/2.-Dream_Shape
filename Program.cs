5d87310b1c97e04577ba00909385d9180aae47f9
Dell-User <Dell-User@DESKTOP-88R8045>
2023-09-11 09:33:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Dream_Shape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("          /|");
+         Console.WriteLine("         / |");
+         Console.WriteLine("        /  |");
+         Console.WriteLine("        ____");
+         Console.ReadLine();
+        }
+    }
+}
